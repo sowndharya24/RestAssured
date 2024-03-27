fcf644e527085359a205e198347524314925426d
sowndharya24 <39838449+sowndharya24@users.noreply.github.com>
2024-03-27 19:53:14 +1100

    Update APITest.java

--- src/test/java/APITest.java
+++ src/test/java/APITest.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 import static io.restassured.RestAssured.*;
 
 public class APITest {
-//https://medium.com/@bubu.tripathy/testing-restful-apis-with-rest-assured-6d245401deea
+
     @Test
     public void testGetRequest() {
         given()
